f82a4031709d4d163b4f2d8435e26f3eb8b8629a
Adam Berrou <adazbebiz@gmail.com>
2024-10-29 17:04:59 +0000

    git

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { v4: uuidv4 } = require('uuid'); // Corrected import
 const mongoose = require('mongoose');
 const { ObjectId } = require('mongodb'); // Import ObjectId
 
-
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
@@ -14,10 +13,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-mongoose.mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', (err) => {
   console.error(`MongoDB connection error: ${err}`);
 });
+
 let userSchema = new mongoose.Schema({
   username: String,
   _id: mongoose.Schema.Types.ObjectId
@@ -31,7 +31,7 @@ app.post('/api/users', (req, res) => {
     _id: new mongoose.Types.ObjectId() // Generate new ObjectId
   });
 
-  newUser.save().then((err, savedUser) => {
+  newUser.save((err, savedUser) => {
     if (err) return res.status(500).json({ error: err });
     res.json({
       username: savedUser.username,
@@ -40,7 +40,6 @@ app.post('/api/users', (req, res) => {
   });
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => 
-{
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});

da8a9117a37400060a3225d69b654b09ca613240
Adam Berrou <adazbebiz@gmail.com>
2024-10-29 16:55:50 +0000

    mongo

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-mongoose.mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.mongoose.connect(process.env.MONGO_URI);
 mongoose.connection.on('error', (err) => {
   console.error(`MongoDB connection error: ${err}`);
 });

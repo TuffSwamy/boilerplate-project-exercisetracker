51b9172fdf7afca2dd671e49938d3fbff90fd09f
Adam Berrou <adazbebiz@gmail.com>
2024-10-29 11:01:39 +0000

    1

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+let users = [];
 
-
-
-
+app.post('api/users', (req, res) => {
+  users.push(req.body.username);
+  res.json({
+    username: req.body.username,
+    _id: req.body.username
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)

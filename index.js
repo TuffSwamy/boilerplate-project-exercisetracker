ec470f47e78050958b91bd4f4f336aba7268d444
Adam Berrou <adazbebiz@gmail.com>
2024-10-29 14:17:26 +0000

    w

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-let users = [];
 
 mongoose.mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', (err) => {
   console.error(`MongoDB connection error: ${err}`);
 });
+let userSchema = new mongoose.Schema({
+  username: String,
+  id: String
+});
+let user = new mongoose.model('User', userSchema);
 
 app.post('/api/users', (req, res) => {
-  let user = req.body.username;
-  let id = uuidv4(); 
-  users.push({ username: req.body.username, id: id });
-  res.json({
-    username: req.body.username,
-    _id:id 
-  });
-});
+  let username = req.body.username;
+  user.save({ username: username, id: new ObjectId() }, (err, data) => {
 
+});
+} );
 const listener = app.listen(process.env.PORT || 3000, () => 
 {
   console.log('Your app is listening on port ' + listener.address().port)

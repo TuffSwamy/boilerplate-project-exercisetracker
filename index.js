25ede55989bed7adedeecb105e44c38f54073290
Adam Berrou <adazbebiz@gmail.com>
2024-10-29 13:49:49 +0000

    mongoDb

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const uid = require('uuid'); 
+const mongoose = require('mongoose');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -11,6 +12,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 let users = [];
+
+mongoose.mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
 
 app.post('api/users', (req, res) => {
   let user = users.push(req.body.username);
